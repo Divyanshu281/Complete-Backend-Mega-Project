098b0208f50bd4cac0cae21e313f89a185ef0db7
Divyanshu281 <divyanshubindal76@gmail.com>
2026-02-14 15:24:00 +0530

    Mongo Database Connected

---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const DB_Name ="MEGA_PROJECT_DB";
---
+++ src/index.js
@@ -0,0 +1,46 @@
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+
+
+dotenv.config({
+    path: './.env',
+    debug: false
+})
+connectDB()
+
+
+
+
+
+
+
+
+
+// import express from "express"
+// app = express();
+
+
+// (
+//    async () =>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGO_URI}/${DB_Name}`)
+//         console.log("Connected to DB");
+//         app.on("Error", (error)=>{
+//             console.log("Error", error);
+//             throw error;
+            
+//         })
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening at port: ${process.env.PORT}`);
+            
+//         })
+        
+
+//     }
+//     catch(error){
+//         console.log("Connection Failed",error);
+
+//     }
+// }
+// )()
